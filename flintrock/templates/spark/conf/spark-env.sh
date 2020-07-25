264e2338232f30df27544a0c978fa1cbc7cdee7c
thekaar <kristjan.kaar@richmond.edu>
2020-07-25 19:49:34 +0100

    Update spark-env.sh

--- flintrock/templates/spark/conf/spark-env.sh
+++ flintrock/templates/spark/conf/spark-env.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Added by me 
+wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
+mv Anaconda3-2020.07-Linux-x86_64.sh Anaconda3
+sh Anaconda3 -y
+
+echo 'export PATH="/home/ec2-user/anaconda3/bin:$PATH"' >> ~/.bashrc
+echo 'export SPARK_HOME="/home/ec2-user/spark"' >> ~/.bashrc
+echo 'export PATH="$PATH:$SPARK_HOME/bin:$PATH"' >> ~/.bashrc
+
 export SPARK_LOCAL_DIRS="{spark_root_ephemeral_dirs}"
 
 # Standalone cluster options
