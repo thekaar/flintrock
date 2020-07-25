cf92cb99296d64a0645eb681adf3c654071943b9
thekaar <kristjan.kaar@richmond.edu>
2020-07-25 20:02:24 +0100

    Update spark-env.sh

--- flintrock/templates/spark/conf/spark-env.sh
+++ flintrock/templates/spark/conf/spark-env.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 # Added by me 
-wget https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
-mv Anaconda3-2020.07-Linux-x86_64.sh Anaconda3
-sh Anaconda3 -y
+sudo yum install gcc gcc-c++ -y
+sudo yum install -y python3
 
-echo 'export PATH="/home/ec2-user/anaconda3/bin:$PATH"' >> ~/.bashrc
-echo 'export SPARK_HOME="/home/ec2-user/spark"' >> ~/.bashrc
-echo 'export PATH="$PATH:$SPARK_HOME/bin:$PATH"' >> ~/.bashrc
+echo 'export PYSPARK_PYTHON=/usr/bin/python3' >> ~/.bashrc
+
 
 export SPARK_LOCAL_DIRS="{spark_root_ephemeral_dirs}"
 
